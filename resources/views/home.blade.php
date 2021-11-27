5d2a540f8404c3ef1a04b2ab95ff8e12495e6a16
Ives Samuel <ivessamuel64@gmail.com>
2021-11-27 11:20:43 -0300

    Inicio Projeto/Template Pronto]

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <div class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Converter/Comprar
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">Histórico</a>
+                        <a href="#" class="list-group-item list-group-item-action">Taxas</a>
+                        <a href="#" class="list-group-item list-group-item-action">Metódos de Pagamento</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
